1d8fb0319a57354a562aba11a4925b1396f9408b
Tushar Gaur <68123758+TusharGaur@users.noreply.github.com>
2024-09-09 22:42:22 +0530

    Update parsePR.js

--- parsePR.js
+++ parsePR.js
@@ -1,25 +1,31 @@
 const fs = require('fs');
-const readline = require('readline')
+const readline = require('readline');
 
 async function extractTests() {
-
-    //by default we specify that all tests should run
+    // by default, specify that all tests should run
     let testsFile = __dirname + '/testsToRun.txt';
-    await fs.promises.writeFile(testsFile, 'all');
+    await fs.promises.writeFile(testsFile, 'null'); // default to null if no tests are found
 
     const lines = readline.createInterface({
         input: fs.createReadStream(__dirname + '/pr_body.txt'),
         crlfDelay: Infinity
     });
 
-    for await (const line of lines) {
-        //special delimeter for apex tests
-        if (line.includes('Apex::[') && line.includes(']::Apex')) {
+    let hasTests = false;
 
+    for await (const line of lines) {
+        // special delimiter for apex tests
+        if (line.includes('Apex::[') && line.includes(']::Apex')) {
             let tests = line.substring(7, line.length - 7);
             await fs.promises.writeFile(testsFile, tests);
             await fs.promises.appendFile(testsFile, '\n');
+            hasTests = true;
         }
+    }
+
+    // If no tests found in the PR body, write 'null' to the file
+    if (!hasTests) {
+        await fs.promises.writeFile(testsFile, 'null');
     }
 }
 
